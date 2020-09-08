a816a37e3beb5a23abc0fadd7e9b2cb13363bcc0
Shubhankar-afk <shubhankar.30-cse-19@mietjammu.in>
2020-09-08 13:25:53 +0530

    removed conio

--- pointerExamination.cpp
+++ pointerExamination.cpp
@@ -1,18 +1,15 @@
 // Accessing the value of variables using poniters
 #include<iostream.h>
-#include<conio.h>
 int main(){
 	int a;
 	float b;
 	int *p;
 	float *q;
-	clrscr();
 	a=10;
 	b=20.5;
 	p=&a;
 	q=&b;
 	cout<<"The value of a and b displayed without pointers are \n"<<a<<b;
 	cout<<"The values of a and b displayed using poniters are\n "<< "  "<<*p<<"  "<<*q;
-	getch();
 	return 0;
 }
