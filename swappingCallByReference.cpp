64ab075f318e30099886d4f447edaad0ddd1abe1
Shubhankar-afk <shubhankar.30-cse-19@mietjammu.in>
2020-09-08 14:37:42 +0530

    more info in output

--- swappingCallByReference.cpp
+++ swappingCallByReference.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 void swapFunction(int *ptrA, int *ptrB){
 	int swap;
+	cout<<"Values before swapping (inside function):\na = "<<*ptrA<<"\nb = "<<*ptrB<<endl;
+	cout<<"Address before swapping (inside function):\na = "<<ptrA<<"\nb = "<<ptrB<<endl;
 	swap=*ptrA;
 	*ptrA=*ptrB;
 	*ptrB=swap;
 	cout<<"Values after swapping (inside function):\na = "<<*ptrA<<"\nb = "<<*ptrB<<endl;
+	cout<<"Address after swapping (inside function):\na = "<<ptrA<<"\nb = "<<ptrB<<endl;
 }
 int main(){
 	int a=10, b=20;
@@ -13,7 +16,9 @@ int main(){
 	ptrA=&a;
 	ptrB=&b;
 	cout<<"Values before swapping (inside main function):\na = "<<a<<"\nb = "<<b<<endl;
+	cout<<"Address before swapping (inside main function):\na = "<<&a<<"\nb = "<<&b<<endl;
 	swapFunction(&a, &b);
 	cout<<"Values after swapping (inside main function):\na = "<<a<<"\nb = "<<b<<endl;
+	cout<<"Address after swapping (inside main function):\na = "<<&a<<"\nb = "<<&b<<endl;
 	return 0;
 }
